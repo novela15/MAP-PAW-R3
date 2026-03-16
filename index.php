23dc33098ad2897008b706a084dd2167cf654764
Unnamed <179119970+gayuhsa@users.noreply.github.com>
2026-03-16 09:12:54 +0700

    One small step towards MVC :D

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+session_start();
+
+require_once __DIR__ . '/config.php';
+require_once CONTROLLERS_PATH . 'AuthController.php';
+
+$page = isset($_GET["page"]) ? $_GET["page"] : "signup";
+$action = isset($_GET["action"]) ? $_GET["action"] : "index";
+
+// I feel this will be a giant switch case when all features are combined, but whatever
+switch ($page) {
+    case "signup":
+        $controller = new AuthController();
+        $controller->signup();
+        break;
+    case "budget-account":
+        include VIEWS_PATH . "budget-account/budget-account.php";
+        break;
+}
+
+?>
