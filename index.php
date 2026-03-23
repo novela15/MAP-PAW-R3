2fb076a842e2cb503a5c72d478769f92081b22b2
Unnamed <179119970+gayuhsa@users.noreply.github.com>
2026-03-23 07:42:32 +0700

    Added try-catch to index.php

--- index.php
+++ index.php
@@ -16,7 +16,11 @@ $page = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 $page = str_replace($script_directory, "", $page);
 $page = trim($page, "/");
 
-$front_controller = new FrontController();
-$front_controller->switch_page($page);
+try {
+    $front_controller = new FrontController();
+    $front_controller->switch_page($page);
+} catch (Exception $exception) {
+    echo "Something went wrong.";
+}
 
 ?>
