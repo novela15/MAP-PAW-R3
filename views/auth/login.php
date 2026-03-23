4db1c9a4fde2eb7876342d0dd892b0fb51fc6b4e
Unnamed <179119970+gayuhsa@users.noreply.github.com>
2026-03-23 12:26:15 +0700

    Added error messages to registration-page

--- views/auth/login.php
+++ views/auth/login.php
@@ -14,24 +14,22 @@
             <form action="login" method="POST">
 		        <div class="input-container">
 		            <p class="input-label">Email</p>
-		            <input class="input email-input" name="email_input" type="email" placeholder="Email" autocomplete="off">
+		            <input class="input email-input" name="email_input" type="email" placeholder="<?php echo $email_error ?? "Email"; ?>">
 		            <p class="input-label">Password</p>
-		            <input class="input password-input" name="password_input" type="password" placeholder="Password">
-		            <a href="" class="forgot-password">Forgot password?</a>
+		            <input class="input password-input" name="password_input" type="password" placeholder="<?php echo $password_error ?? "Password"; ?>">
+		            <a href="" class="forgot-password">Lupa password?</a>
 		        </div>
 
 		        <div class="input-container button-container">
-		            <button class="login-email">Sign In</button>
+		            <button class="login-email" name="login_email">Sign In</button>
 		            <div>or</div>
-		            <button class="login-google">Sign in with<img src="https://www.google.com/favicon.ico" loading="lazy"></button>
+		            <button class="login-google" name="login_google">Sign in with<img src="https://www.google.com/favicon.ico" loading="lazy"></button>
 		        </div>
             </form>
 
             <div class="sign-in-link">
-                <a href="signup">No account? Sign up.</a>
+                <a href="signup">Belum punya akun? Daftar.</a>
             </div>
         </div>
-
-        <!--<script src="registration.js"></script>-->
     </body>
 </html>
--- views/auth/signup.php
+++ views/auth/signup.php
@@ -14,27 +14,25 @@
             <form action="signup" method="POST">
 		        <div class="input-container">
 		            <p class="input-label">Username</p>
-		            <input class="input username-input" name="username_input" type="text" placeholder="Username" autocomplete="off">
+		            <input class="input username-input" name="username_input" type="text" placeholder="<?php echo $username_error ?? "Username"; ?>">
 		            <p class="input-label">Email</p>
-		            <input class="input email-input" name="email_input" type="email" placeholder="Email" autocomplete="off">
+		            <input class="input email-input" name="email_input" type="email" placeholder="<?php echo $email_error ?? "Email"; ?>">
 		            <p class="input-label">Password</p>
-		            <input class="input password-input" name="password_input" type="password" placeholder="Password">
-		            <p class="input-label">Confirm Password</p>
-		            <input class="input password-confirm-input" name="password_confirm_input" type="password" placeholder="Confirm Password">
+		            <input class="input password-input" name="password_input" type="password" placeholder="<?php echo $password_error ?? "Password (minimal 6 karakter)"; ?>">
+		            <p class="input-label">Konfirmasi Password</p>
+		            <input class="input password-confirm-input" name="password_confirm_input" type="password" placeholder="<?php echo $password_error ?? "Konfirmasi Password"; ?>">
 		        </div>
 
 		        <div class="input-container button-container">
 		            <button class="signup-email" name="signup_email" type="submit">Sign Up</button>
 		            <div>or</div>
-		            <button class="signup-google" name="signup_google" type="submit">Sign up with<img src="https://www.google.com/favicon.ico" loading="lazy"></button>
+		            <button class="signup-google" name="signup_google" type="submit">Sign up dengan<img src="https://www.google.com/favicon.ico" loading="lazy"></button>
 		        </div>
             </form>
 
             <div class="sign-in-link">
-                <a href="login">Have an account? Sign in.</a>
+                <a href="login">Sudah punya akun? Masuk.</a>
             </div>
         </div>
-
-        <!--<script src="registration.js"></script>-->
     </body>
 </html>
