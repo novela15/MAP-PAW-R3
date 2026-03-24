34a4a53a2e2433295c56bb1387ff9f1e15e9893e
Unnamed <179119970+gayuhsa@users.noreply.github.com>
2026-03-24 20:08:09 +0700

    Added hardcoded page titles

--- views/auth/login.php
+++ views/auth/login.php
@@ -4,6 +4,9 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>MAP - Login</title>
+
         <link rel="stylesheet" href="<?php echo VIEWS_PATH; ?>auth/auth.css">
     </head>
 
--- views/auth/signup.php
+++ views/auth/signup.php
@@ -4,6 +4,9 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>MAP - Sign Up</title>
+
         <link rel="stylesheet" href="<?php echo VIEWS_PATH; ?>auth/auth.css">
     </head>
 
