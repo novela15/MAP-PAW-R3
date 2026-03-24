7bc5a7bc39d4875e5098d344a1b03fe53924888b
Unnamed <179119970+gayuhsa@users.noreply.github.com>
2026-03-24 20:06:09 +0700

    Added page titles

--- controllers/FrontController.php
+++ controllers/FrontController.php
@@ -24,34 +24,42 @@ class FrontController {
                 break;
             case "budget-book": // Still doesn't exist yet
                 $page_content = VIEWS_PATH . "budget-book/budget-book.php";
+                $page_title = "Budget Book";
                 require_once SKELETON_PATH . "skeleton.php";
                 break;
             case "budget-category": // Still doesn't exist yet
                 $page_content = VIEWS_PATH . "budget-category/budget-category.php";
+                $page_title = "Budget Category";
                 require_once SKELETON_PATH . "skeleton.php";
                 break;
             case "budget-account":
                 $page_content = VIEWS_PATH . "budget-account/budget-account.php";
+                $page_title = "Budget Account";
                 require_once SKELETON_PATH . "skeleton.php";
                 break;
             case "record-expense": // Still doesn't exist yet
                 $page_content = VIEWS_PATH . "record-expense/record-expense.php";
+                $page_title = "Record Expense";
                 require_once SKELETON_PATH . "skeleton.php";
                 break;
             case "general-journal": // Still doesn't exist yet
                 $page_content = VIEWS_PATH . "general-journal/general-journal.php";
+                $page_title = "General Journal";
                 require_once SKELETON_PATH . "skeleton.php";
                 break;
             case "general-ledger": // Still doesn't exist yet
                 $page_content = VIEWS_PATH . "general-ledger/general-ledger.php";
+                $page_title = "General Ledger";
                 require_once SKELETON_PATH . "skeleton.php";
                 break;
             case "budget-realization": // Still doesn't exist yet
                 $page_content = VIEWS_PATH . "budget-realization/budget-realization.php";
+                $page_title = "Budget Realization";
                 require_once SKELETON_PATH . "skeleton.php";
                 break;
             case "close-book": // Still doesn't exist yet
                 $page_content = VIEWS_PATH . "close-book/close-book.php";
+                $page_title = "Close Book";
                 require_once SKELETON_PATH . "skeleton.php";
                 break;
             default:
