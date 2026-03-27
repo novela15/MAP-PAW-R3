073e882308d127ec17a52e9ba1da9c2bf0d79e30
Unnamed <179119970+gayuhsa@users.noreply.github.com>
2026-03-27 21:14:22 +0700

    Created AuthHelper

---
+++ utilities/AuthHelper.php
@@ -0,0 +1,20 @@
+<?php
+
+class AuthHelper {
+    public function __construct() {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+    }
+
+    public function isLoggedIn(): bool {
+        return isset($_SESSION["user_id"]) && !empty($_SESSION["user_id"]);
+    }
+
+    public function updateSession(string $id, string $name) {
+        $_SESSION["user_id"] = $id;
+        $_SESSION["user_name"] = $name;
+    }
+}
+
+?>
