b22125de11f3bb93ad40e4a44fb4e41e6db1c806
Unnamed <179119970+gayuhsa@users.noreply.github.com>
2026-03-27 21:37:00 +0700

    Added destroySession()

--- utilities/AuthHelper.php
+++ utilities/AuthHelper.php
@@ -11,6 +11,11 @@ class AuthHelper {
         return isset($_SESSION["user_id"]) && !empty($_SESSION["user_id"]);
     }
 
+    public function destroySession() {
+        session_unset();
+        session_destroy();
+    }
+
     public function updateSession(string $id, string $name) {
         $_SESSION["user_id"] = $id;
         $_SESSION["user_name"] = $name;
