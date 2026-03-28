08b7590b7500eeaf291e2f7718382b381b42239f
Unnamed <179119970+gayuhsa@users.noreply.github.com>
2026-03-28 20:07:16 +0700

    Added autoloader

---
+++ app/utilities/Autoloader.php
@@ -0,0 +1,25 @@
+<?php
+
+class Autoloader {
+    private static array $directories = [
+        CONTROLLERS_PATH,
+        CORE_PATH,
+        MODELS_PATH,
+        UTILITIES_PATH
+    ];
+
+    public static function register(): void {
+        spl_autoload_register(function($class) {
+            foreach (self::$directories as $directory) {
+                $script = $directory . $class . ".php";
+
+                if (file_exists($script)) {
+                    require_once $script;
+                    return;
+                }
+            }
+        });
+    }
+}
+
+?>
