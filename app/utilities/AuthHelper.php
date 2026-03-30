8e573e1f2fe3425f90561fef8c59baf1617b903a
Unnamed <179119970+gayuhsa@users.noreply.github.com>
2026-03-30 09:54:59 +0700

    Made session management more secure

--- app/utilities/AuthHelper.php
+++ app/utilities/AuthHelper.php
@@ -12,11 +12,22 @@ class AuthHelper {
     }
 
     public function destroySession() {
-        session_unset();
+        $_SESSION = array();
+
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+
+            setcookie(session_name(), '', time() - 42000,
+                $params["path"], $params["domain"],
+                $params["secure"], $params["httponly"]
+            );
+        }
+
         session_destroy();
     }
 
     public function updateSession(string $id, string $name) {
+        session_regenerate_id(true);
         $_SESSION["user_id"] = $id;
         $_SESSION["user_name"] = $name;
     }
