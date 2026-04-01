767694f954969ab2140e47f20f65eafc525d868e
Unnamed <179119970+gayuhsa@users.noreply.github.com>
2026-04-01 18:57:58 +0700

    Added model for budget account

---
+++ app/models/BudgetAccountModel.php
@@ -0,0 +1,33 @@
+<?php
+
+class BudgetAccountModel {
+    private $db;
+
+    public function __construct() {
+        $this->db = Database::getInstance();
+    }
+
+    public function create(array $data): array {
+        $this->db->query(
+            "INSERT INTO budget_accounts (user_id, name, category_id, description, volume, unit, amount) VALUES (?, ?, ?, ?, ?, ?, ?)",
+            [
+                $data["user_id"],
+                $data["name"],
+                $data["category_id"],
+                $data["description"],
+                $data["volume"],
+                $data["unit"],
+                $data["amount"]
+            ]
+        );
+
+        return $this->getByUserId($this->db->getConnection()->lastInsertId());
+    }
+
+    public function getAllByUserId(int $id): array {
+        $statement = $this->db->query("SELECT * FROM budget_accounts WHERE user_id = ?", [$id]);
+        return $statement->fetch() ?: [];
+    }
+}
+
+?>
