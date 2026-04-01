4657e2c0f4ff244da1d88cf13a256f9484c65bc3
Unnamed <179119970+gayuhsa@users.noreply.github.com>
2026-04-01 18:58:40 +0700

    Added budget_accounts table to SQL setup script

--- app/core/database_setup.sql
+++ app/core/database_setup.sql
@@ -9,3 +9,16 @@ CREATE TABLE map_paw_r3.users (
     password VARCHAR(255) DEFAULT NULL, -- Actually this is the password hash
     registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+-- TODO: volume, unit, and amount should be joined from budget_category
+CREATE TABLE map_paw_r3.budget_accounts (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    user_id INT UNSIGNED NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    category_id INT UNSIGNED NOT NULL, -- Foreign key
+    description TEXT,
+    volume DECIMAL(15, 2) DEFAULT 0 CHECK (volume >= 0),
+    unit VARCHAR(100) NOT NULL DEFAULT 'pcs',
+    amount DECIMAL(15, 2) DEFAULT 0 CHECK (amount >= 0),
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES map_paw_r3.users(id) ON DELETE CASCADE
+);
