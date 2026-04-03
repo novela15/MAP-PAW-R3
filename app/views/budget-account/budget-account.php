538c150740c6f80b28cb1f810286653ef686a9ad
Unnamed <179119970+gayuhsa@users.noreply.github.com>
2026-04-03 19:05:05 +0700

    Added empty table placeholder

--- app/views/budget-account/budget-account.php
+++ app/views/budget-account/budget-account.php
@@ -1,4 +1,4 @@
-            <link rel="stylesheet" href="frontend/budget-account/budget-account.css">
+            <link rel="stylesheet" href="frontend/budget-account/budget-account.css?v=<?php echo time();?>">
 
             <p class="container-header">Akun Anggaran</p>
             <div class="subcontainer">
@@ -19,17 +19,21 @@
                         <th>Jumlah</th>
                         <th>Action</th>
                     </tr>
-                    <?php foreach ($budgetAccountTables as $row): ?>
-                        <tr>
-                            <td><?= htmlspecialchars($row["name"]) ?></td>
-                            <td><?= htmlspecialchars($row["category"]) ?></td>
-                            <td><?= htmlspecialchars($row["description"]) ?></td>
-                            <td><?= htmlspecialchars($row["volume"]) ?></td>
-                            <td><?= htmlspecialchars($row["unit"]) ?></td>
-                            <td>Rp<?= number_format($row["amount"], 2, ',', '.') ?></td>
-                            <td><div class="table-action"><button class="delete-button trash-can-button"><i class="fa-regular fa-trash-can"></i></button><button class="write-button pen-button"><i class="fa-solid fa-pen"></i></i></button></div></th>
-                        </tr>
-                    <?php endforeach; ?>
+                    <?php if (!empty($budgetAccountTables)): ?>
+                        <?php foreach ($budgetAccountTables as $row): ?>
+                            <tr>
+                                <td><?= htmlspecialchars($row["name"]) ?></td>
+                                <td><?= htmlspecialchars($row["category"]) ?></td>
+                                <td><?= htmlspecialchars($row["description"]) ?></td>
+                                <td><?= htmlspecialchars($row["volume"]) ?></td>
+                                <td><?= htmlspecialchars($row["unit"]) ?></td>
+                                <td>Rp<?= htmlspecialchars(number_format($row["amount"], 2, ',', '.')) ?></td>
+                                <td><div class="table-action"><button class="delete-button trash-can-button"><i class="fa-regular fa-trash-can"></i></button><button class="write-button pen-button"><i class="fa-solid fa-pen"></i></i></button></div></th>
+                            </tr>
+                        <?php endforeach; ?>
+                    <?php else: ?>
+                        <td class="empty-table-placeholder" colspan="7">Tidak ada akun anggaran yang ditemukan.</td>
+                    <?php endif; ?>
                 </table>
                 <div class="bottom-action"><button class="delete-button trash-can-button"><i class="fa-regular fa-trash-can"></i></button><button class="write-button pen-button"><i class="fa-solid fa-pen"></i></i></button></div>
             </div>
