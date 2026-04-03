9f4389fed2dca3c8d99aec184d29c45610377581
Unnamed <179119970+gayuhsa@users.noreply.github.com>
2026-04-03 19:09:18 +0700

    Converted error view logic to alternative syntax

--- app/views/error-pages/error.php
+++ app/views/error-pages/error.php
@@ -5,36 +5,30 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <?php
-        if (isset($exception) && http_response_code() === 404) {
-            echo "<title>MAP - Error 404</title>";
-        } else {
-            echo "<title>MAP - Error 500</title>";
-        }
-        ?>
+        <?php if (isset($exception) && http_response_code() === 404): ?>
+            <title>MAP - Error 404</title>
+        <?php else: ?>
+            <title>MAP - Error 500</title>
+        <?php endif; ?>
 
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico">
         <link rel="stylesheet" href="frontend/error-pages/error.css?v=<?php echo time();?>">
     </head>
 
     <body>
-        <?php
-        if (isset($exception) && http_response_code() === 404) {
-            echo '<p class="error-header">404</p>';
-            echo '<p class="error-subheader">Halaman tidak ditemukan</p>';
-        } else {
-            echo '<p class="error-header">500</p>';
-            echo '<p class="error-subheader">Masalah internal server</p>';
-        }
-        ?>
+        <?php if (isset($exception) && http_response_code() === 404): ?>
+            <p class="error-header">404</p>
+            <p class="error-subheader">Halaman tidak ditemukan</p>
+        <?php else: ?>
+            <p class="error-header">500</p>
+            <p class="error-subheader">Masalah internal server</p>
+        <?php endif; ?>
 
         <a href="javascript:history.back()">Kembali ke halaman sebelumnya</a>
 
-        <?php
-        if (ENVIRONMENT === "dev" && isset($exception)) {
-            require_once ERROR_PAGES_PATH . "message.php";
-        }
-        ?>
+        <?php if (ENVIRONMENT === "dev" && isset($exception)): ?>
+            <?php require_once ERROR_PAGES_PATH . "message.php"; ?>
+        <?php endif; ?>
     </body>
 </html>
 
