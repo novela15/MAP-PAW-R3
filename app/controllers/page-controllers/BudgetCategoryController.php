23b16c65ff533fc378f3fd147e6f2cfb04e9ec8a
Unnamed <179119970+gayuhsa@users.noreply.github.com>
2026-04-08 22:51:34 +0700

    Update BudgetCategoryController

--- app/controllers/page-controllers/BudgetCategoryController.php
+++ app/controllers/page-controllers/BudgetCategoryController.php
@@ -1,14 +1,54 @@
 <?php
 
 class BudgetCategoryController extends FeaturePageController {
+    private function add() {
+        $budgetCategoryModel = new BudgetCategoryModel();
+
+        $_POST["user_id"] = $_SESSION["user_id"];
+        $budgetCategoryModel->create($_POST);
+    }
+
+    private function delete() {
+        $budgetCategoryModel = new BudgetCategoryModel();
+
+        $_POST["user_id"] = $_SESSION["user_id"];
+        $budgetCategoryModel->deleteById((int)$_POST["item_id"]);
+    }
+
+    private function edit() {
+        $budgetCategoryModel = new BudgetCategoryModel();
+
+        $_POST["user_id"] = $_SESSION["user_id"];
+        $_POST["id"] = $_POST["item_id"];
+        $budgetCategoryModel->update($_POST);
+    }
+
     public function index() {
+        $budgetCategoryModel = new BudgetCategoryModel();
+
         $this->renderView(
             "budget-category/budget-category",
-            "Budget Category"
+            "Budget Category",
+            ["table" => $budgetCategoryModel->getAllByUserId($_SESSION["user_id"])]
         );
     }
 
     public function post() {
+        if (!isset($_POST) || !isset($_POST["type"])) { return; }
 
+        switch ($_POST["type"]) {
+            case "add":
+                $this->add();
+                break;
+            case "delete":
+                $this->delete();
+                break;
+            case "edit":
+                $this->edit();
+                break;
+        }
+
+        header("Location: budget-category");
+        exit;
     }
 }
